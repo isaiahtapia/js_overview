bab0bad2abd0f6dcc154f16798fae8469b9784a4
Isaiahtapia <isaiahjtapia@gmail.com>
2024-06-18 10:23:13 -0400

    finished console basics

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// Javavscript works with or has a set of different values 
+        // a word or multiple words wrapped in quotes is called a String value type
+        // a number is called a Number value type
+        var firstName = "Isaiah";
+
+        console.log(3.2);
+
+        console.log(5 * 5);
+
+        console.log(firstName);
