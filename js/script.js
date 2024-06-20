1a96369d139c6fd8bd7b824ba9a873325bec9b0e
Isaiahtapia <isaiahjtapia@gmail.com>
2024-06-20 12:58:05 -0400

    almost finished with loops'

--- js/script.js
+++ js/script.js
@@ -1,11 +1,20 @@
-// Javavscript works with or has a set of different values 
-        // a word or multiple words wrapped in quotes is called a String value type
-        // a number is called a Number value type
 
-        // Primitive Value Types
-        var firstName = "Isaiah"; // String
-        var lastName = "Tapia"
-        var age = 23; //Number 
-        var names = ['John', 'Ann', 'Bob', 'Frank', 'Jane','Jimmy', 'Billy','Julie', 'Will']; //Array
+var names = ['John', 'Ann', 'Bob', 'Frank', 'Jane','Jimmy', 'Billy','Julie', 'Will']; //Array
+var users = ['Bob', 99, 'Ann', 35, 'Billy', 45]
 
-        
+// index = reference point (or items) in an Array
+// Print a count from 1 to 10
+// for (var count = 1; count <= 10; count++) 
+
+// for (var index = 0; index < names.length; index++) {
+//         console.log(names[index]);
+// }
+
+var count = 1;
+while (count <= 10) {
+        console.log(count);
+
+        count++;
+}
+
+
