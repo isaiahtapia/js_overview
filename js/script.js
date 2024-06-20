5835441f6054e847418cffb77091f2e5c0638914
Isaiahtapia <isaiahjtapia@gmail.com>
2024-06-20 09:49:15 -0400

    variables and data type basics as well as array basics

--- js/script.js
+++ js/script.js
@@ -1,10 +1,63 @@
 // Javavscript works with or has a set of different values 
         // a word or multiple words wrapped in quotes is called a String value type
         // a number is called a Number value type
-        var firstName = "Isaiah";
+        var firstName = "Isaiah"; // String
+        var lastName = "Tapia"
+        var age = 23; //Number 
+        var isADev = true;//Boolean
+        var un = undefined; // Undefined is a value type that JS will give you when there is no declared or assigned value
+        var n = null; //Programmer's representation of nothing
+        var notANumber = NaN; //Not a Number - This is provided when you try to process a mathematical equation and the result is not a valid number
 
-        console.log(3.2);
+        var nothing;
+        var another;
+        var oneMore;
 
-        console.log(5 * 5);
+        // console.log(age); // Using a variable within your code is called a reference
 
-        console.log(firstName);
+        // age = age + 1;
+        // age += 5; //Increase a variable by any numerical amount 
+        age++; //Increase a variable by one
+
+        // console.log(age);
+
+        age--;//Decrease a variable by one 
+
+        // console.log(age);
+
+        // age -= 5;
+
+        var fullName = firstName + " "+ lastName;
+         
+        var names = ['John', 'Ann', 'Bob', 'Frank', 'Jane','Jimmy', 'Billy','Julie', 'Will']; //Array
+
+        var ages = [30,24, 55, 99];
+
+
+        // names.push('Susan');
+
+
+        // console.log(names);
+
+        // names.unshift('Eli');
+
+        console.log(names);
+
+        // names.pop();
+        // names.pop();
+        names.splice(5,1);
+
+        console.log(names);
+
+        // console.log(names[names.length - 1]);
+
+        // console.log(names);
+
+        // names[1] = 'Joe';
+
+        // console.log(names);
+
+        // ages[2] =+ 10;
+
+        // console.log(ages);
+        
