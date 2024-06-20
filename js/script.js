c633a75458ed4d558e444781af7a02d6cb67e2d9
Isaiahtapia <isaiahjtapia@gmail.com>
2024-06-20 13:02:27 -0400

    new branch

--- js/script.js
+++ js/script.js
@@ -1,20 +1,2 @@
 
-var names = ['John', 'Ann', 'Bob', 'Frank', 'Jane','Jimmy', 'Billy','Julie', 'Will']; //Array
-var users = ['Bob', 99, 'Ann', 35, 'Billy', 45]
-
-// index = reference point (or items) in an Array
-// Print a count from 1 to 10
-// for (var count = 1; count <= 10; count++) 
-
-// for (var index = 0; index < names.length; index++) {
-//         console.log(names[index]);
-// }
-
-var count = 1;
-while (count <= 10) {
-        console.log(count);
-
-        count++;
-}
-
-
+var num = 10;
