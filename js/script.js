34dd834e085461bbcdcf65881f8384381f846217
Isaiahtapia <isaiahjtapia@gmail.com>
2024-06-21 10:19:16 -0400

    finished functions basic

---
+++ js/script.js
@@ -0,0 +1,31 @@
+
+// function myFirstFunc() {
+//         console.log('function has been called');
+// }
+
+// myFirstFunc();
+
+// function add(num1, num2) { //argument
+//         console.log(num1 + num2);
+// }
+
+// add(5,10); //parameter
+
+// function printValues (num1, name, boolean, another) {
+//         console.log(another);
+// }
+
+// printValues (10, 'Allen', false);
+ 
+function printNames(names) {
+     for (const name of names) {
+     console.log(name);
+        }
+}
+// printNames(['Bob','Jimmy','Julie']);
+
+const someName = "Isaiah Tapia";
+const h1 = document.querySelector('h1');
+
+h1.innerText = someName;
+       
