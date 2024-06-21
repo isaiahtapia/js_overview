73208b4ea3139136e59c576f1c32607c16c452c8
Isaiahtapia <isaiahjtapia@gmail.com>
2024-06-21 12:40:12 -0400

    calculator done with notes

--- js/script.js
+++ js/script.js
@@ -9,30 +9,32 @@ Example: If the operator is a '+' then you will add the two numbers together and
 
 // Call your function 4 times with different numbers and operators to confirm that it is working correctly
 
-function calculate(num1, num2, operator){
-        switch(operator) {
-                case '+':
-                console.log(num1 + num2);
-                break;
-                case '-':
-                console.log(num1 - num2);
-                break;
-                case '*':
-                console.log(num1 * num2);
-                break;
-                case '/':
-                console.log(num1 / num2);
-                break;
-                default:
-                console.log('You must provide a valid operator');
-        }
-}
-calculate(10, 20, '+');
-calculate(2, 10, '-');
-calculate(25, 2, '*');
-calculate(9, 3, '//');
-calculate(81, 3, '/');
-calculate(9, 3, '--');
+//Second way JD did it:
+// function calculate(num1, num2, operator){
+//         switch(operator) {
+//                 case '+':
+//                 console.log(num1 + num2);
+//                 break;
+//                 case '-':
+//                 console.log(num1 - num2);
+//                 break;
+//                 case '*':
+//                 console.log(num1 * num2);
+//                 break;
+//                 case '/':
+//                 console.log(num1 / num2);
+//                 break;
+//                 default:
+//                 console.log('You must provide a valid operator');
+//         }
+// }
+// calculate(10, 20, '+');
+// calculate(2, 10, '-');
+// calculate(25, 2, '*');
+// calculate(9, 3, '//');
+// calculate(81, 3, '/');
+// calculate(9, 3, '--');
+
 //How JD did  it way 1: 
 //function calculate(num1, num2, operator){
 //         if (operator === '+') {
@@ -70,3 +72,5 @@ calculate(9, 3, '--');
 //         console.log(calculate(10, 5, '+'));
 //         console.log(calculate(3, 5, '*'));
 //         console.log(calculate(4, 8, '/'));
+
+
