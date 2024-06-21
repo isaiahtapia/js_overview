94a71c03d101498c301e715f4242b425462ab2c2
Isaiahtapia <isaiahjtapia@gmail.com>
2024-06-21 10:39:41 -0400

    calculator code

--- js/script.js
+++ js/script.js
@@ -1,10 +1,2 @@
 
-/*
-Create a function that takes 3 arguments
--The first and second argument will be a number 
--The third argument will be an mathmatical operator (+, -, *, /)
-
-Example: If the operator is a '+' then you will add the two numbers together and console log the sum */
-
-// Call your function 4 times with different numbers and operators to confirm that it is working correctly
-
+        
